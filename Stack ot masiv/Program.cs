86d1caff8fc5e0b748d92d2761e1299d66922402
Milenski <milenpramatarov@gmail.com>
2024-12-01 21:40:28 +0200

    Add project files.

---
+++ Stack ot masiv/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Stack_ot_masiv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> stack = new Stack<int>(array);
+            int count = stack.Count + 1;
+            while (stack.Count != 0)
+            {
+                count--;
+                Console.WriteLine($"element: {count} ==> {stack.Pop()}");
+
+            }
+        }
+    }
+}
